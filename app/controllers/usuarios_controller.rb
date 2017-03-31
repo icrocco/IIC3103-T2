6eef77e8956cc06a7cfadd14f8e650b38d8b4939
icrocco <ijcrocco@uc.cl>
2017-03-30 23:41:03 -0300

    update method fixed

--- app/controllers/usuarios_controller.rb
+++ app/controllers/usuarios_controller.rb
@@ -1,5 +1,5 @@
 class UsuariosController < ApplicationController
-  before_action :set_usuario, only: [:show, :destroy]
+  before_action :set_usuario, only: [:show, :update, :destroy]
 
   # GET /usuario
   def index
