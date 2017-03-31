b2a7fc6c9092367b49e287b80c7af9b944bd0e79
icrocco <ijcrocco@uc.cl>
2017-03-31 01:26:26 -0300

    general get workin 100%?

--- db/migrate/20170328152532_create_usuarios.rb
+++ db/migrate/20170328152532_create_usuarios.rb
@@ -1,9 +1,9 @@
 class CreateUsuarios < ActiveRecord::Migration[5.0]
   def change
     create_table :usuarios do |t|
-      t.string :usuario
       t.string :nombre
       t.string :apellido
+      t.string :usuario
       t.string :twitter
     end
   end
