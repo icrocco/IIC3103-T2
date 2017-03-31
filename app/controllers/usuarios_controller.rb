9470afb889eb6816698ca24fd36df4e9d3018577
icrocco <ijcrocco@uc.cl>
2017-03-31 00:45:54 -0300

    not showing created_at in single get

--- app/controllers/usuarios_controller.rb
+++ app/controllers/usuarios_controller.rb
@@ -4,7 +4,7 @@ class UsuariosController < ApplicationController
   # GET /usuario
   def index
     @usuarios = Usuario.all
-    json_response({id: @usuario.id, nombre: @usuario.nombre, apellido: @usuario.apellido, usuario: @usuario.usuario, twitter: @usuario.twitter}, :created)
+    json_response(@usuarios)
   end
 
   # PUT /usuario
@@ -15,7 +15,7 @@ class UsuariosController < ApplicationController
 
   # GET /usuarios/:id
   def show
-    json_response(@usuario)
+    json_response({id: @usuario.id, nombre: @usuario.nombre, apellido: @usuario.apellido, usuario: @usuario.usuario, twitter: @usuario.twitter})
   end
 
   # POST /usuario/:id
